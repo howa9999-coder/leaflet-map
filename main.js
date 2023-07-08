1a905c90d876914807c59e65c379440023e83329
howa9999-coder <wahibaadidi1@gmail.com>
2023-07-08 11:58:33 +0100

    leaflet-map

---
+++ main.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([33, -7], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
